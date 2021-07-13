33b11297ac15cd9fcdca81f70747f009339e62fd
302471 <86583259+302471@users.noreply.github.com>
2021-07-13 18:18:02 +0530

    Update testcase.c

--- 3_implementation/test/testcase.c
+++ 3_implementation/test/testcase.c
@@ -1,76 +1,26 @@
-#include "../unity/unity.h"
-#include "../unity/unity_internals.h"
-#include"../inc/ATM.h"
-
-//#include "ATM.h"
-
-
-
-/*void login();
-void mainMenu();
-void checkBalance(float balance);
-float moneyDeposit(float balance);
-float moneyWithdraw(float balance);
-void menuExit();
-void errorMessage();*/
-
+#include "unity.h"
+#include "atm.h"
+#include "unity_internals.h"
 
 /* Required by the unity test framework */
-void setUp()
-{
-}
+void setUp(){}
 /* Required by the unity test framework */
-void tearDown()
+void tearDown(){}
+
+struct customer c[1]={{101,"Sushma",5000}};
+void test_search(void)
 {
-    
+  TEST_ASSERT_EQUAL(0, search(c,1,101));
+  TEST_ASSERT_EQUAL(-1, search(c,1,0));
 }
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
 
-
-
-
-void test_BALANCE_INFO(void){
-TEST_ASSERT_EQUAL(1,BALANCE_INFO(15000));
-
+/* Run Test functions */
+  RUN_TEST(test_search);
+ 
+  /* Close the Unity Test Framework */
+  return UNITY_END(); 
 }
-
-void test_MONEYDEPOSIT_INFO(void){
-TEST_ASSERT_EQUAL(1,MONEYDEPOSIT_INFO(15000+DEPOSIT));
-}
-
-void test_MONEYWITHDRAW_INFO(void){
-TEST_ASSERT_EQUAL(1,MONEYWITHDRAW_INFO(15000-WITHDRAW));
-}
-
-
-
-
-int main(void){
-/*Initiate the unity Test Framework */
-UNITY_BEGIN();
-
-
-
-/* Run Test Function */
-
-/*RUN_TEST(Check_input_main_menu);
-RUN_TEST(check_choose_module);
-RUN_TEST(check_input_checkBalance);
-RUN_TEST(check_input_moneyDeposit);
-RUN_TEST(check_input_moneyWithdraw);
-RUN_TEST(check_menuExit);
-RUN_TEST(check_errorMessage);
-*/
-
-RUN_TEST(void test_BALANCE_INFO(void));
-RUN_TEST(void test_MONEYDEPOSIT_INFO(void));
-
-
-
-RUN_TEST(void test_MONEYWITHDRAW_INFO(void));
-
-
-
- /* Close the Unity Test Framework */
-    return UNITY_END();
-}
-
